c149f42a0ce9e20054357734e7e89338f666048c
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-07-29 12:33:23 -0400

    create: template

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Header from "./components/header/Header.vue";
+</script>
+
+<template>
+  <header>
+    <Header />
+  </header>
+</template>
---
+++ frontend/src/components/header/Header.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div>
+      <img src="/logo.png" alt="" class="w-28 h-24 mx-5 my-7">
+      <div>
+        <p>PREFEITURA DO MUNICÍPIO DE PORTO VELHO</p>
+        <p>Comissão de Ética Pública</p>
+      </div>
+    </div>
+    <hr class="bg-zinc-950 mt-2">
+  </header>
+</template>

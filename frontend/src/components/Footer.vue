0b2a8fc242ff88465a1f4ad70b796248b497f3ca
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2024-11-05 10:42:44 -0400

    feat: update in form and footer

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="bg-gray-800 text-white py-4 text-center">
+    <p>&copy; 2024. Todos os direitos reservados a CGM - Controladoria Geral do Município.</p>
+  </footer>
+</template>

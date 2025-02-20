94e4749b9f96c1f18d74d6ccd5905e1a16386de7
Equipe CGM/PVH <cgm.ti.pvh@gmail.com>
2025-02-20 13:31:43 -0400

    vercel hello world

--- backend-jhon/api/index.js
+++ backend-jhon/api/index.js
@@ -1,35 +1,8 @@
-import cors from "cors";
-import express from "express";
-import serverless from "serverless-http";
-
-// Cria uma instância do Express
+const express = require("express");
 const app = express();
 
-// Habilita CORS para aceitar todas as origens (*)
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST"], 
-};
-app.use(cors(corsOptions));
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Define a rota padrão GET que retorna "Hello World"
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.get("/api", (req, res) => {
-  res.send("Hello World");
-});
-
-// Exporta a função handler para ser usada pelo Vercel
-const handler = serverless(app);
-
-// Inicia o servidor localmente (apenas para desenvolvimento)
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
-export default handler;
+module.exports = app;

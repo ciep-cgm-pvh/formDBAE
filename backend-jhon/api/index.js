fec7686eedae2513078fee648cda6c22b5c7c37a
Equipe CGM/PVH <cgm.ti.pvh@gmail.com>
2025-02-20 13:25:05 -0400

    ANOTHER TRY

--- backend-jhon/api/index.js
+++ backend-jhon/api/index.js
@@ -6,11 +6,29 @@ import serverless from "serverless-http";
 const app = express();
 
 // Habilita CORS para aceitar todas as origens (*)
-const corsOptions = {
+/*const corsOptions = {
   origin: "*",
   methods: ["GET", "POST"], 
 };
-app.use(cors(corsOptions));
+app.use(cors(corsOptions));*/
+app.use((req, res, next) => {
+  const origin = req.get('referer');
+  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+  if (isWhitelisted) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  }
+  // Pass to next layer of middleware
+  if (req.method === 'OPTIONS') res.sendStatus(200);
+  else next();
+});
+const setContext = (req, res, next) => {
+  if (!req.context) req.context = {};
+  next();
+};
+app.use(setContext);
 
 // Define a rota padrão GET que retorna "Hello World"
 app.get("/", (req, res) => {

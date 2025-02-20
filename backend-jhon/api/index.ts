32f936cf9531523cd36bcfae1f3db4d94fe4c74f
Equipe CGM/PVH <cgm.ti.pvh@gmail.com>
2025-02-20 13:45:18 -0400

    finally hello world i guess

--- backend-jhon/api/index.ts
+++ backend-jhon/api/index.ts
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 
 app.use(express.static('public'));
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+// Habilita CORS para aceitar todas as origens (*)
+app.use(cors());
+
+// Define a rota padrão GET que retorna "Hello World"
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 

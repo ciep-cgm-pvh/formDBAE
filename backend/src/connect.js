b85fc2c6e63f550e42324a8961d311185ff9337b
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2025-02-20 08:21:59 -0400

    fix config vercel with connect

--- backend/src/connect.js
+++ backend/src/connect.js
@@ -1,15 +1,20 @@
 import { MongoClient } from 'mongodb'
 
-const URI =
-  'mongodb+srv://cgmtipvh:ydaZOTLaS2tBTmRR@formdbae.ioaql.mongodb.net/?retryWrites=true&w=majority&appName=formDBAE'
+const URI = process.env.MONGODB_URI
+
+if (!URI) {
+  throw new Error('A variável de ambiente MONGODB_URI não está definida.')
+}
+
 const client = new MongoClient(URI)
 
 export const connectToDatabase = async () => {
   try {
     await client.connect()
+    console.log('Conexão bem-sucedida ao MongoDB')
     return client.db('formDBAE') 
   } catch (error) {
     console.error('Erro ao conectar ao MongoDB:', error)
-    throw error
+    throw error 
   }
 }

4851ae6d6fe15aef21fb93e050a31ebee2ece777
JhonatanCarvalh0 <114593875+jhonatanCarvalh0@users.noreply.github.com>
2025-05-14 13:22:05 -0400

    taiwlind form - raw

--- frontend/src/app/(routes)/form-jhonatan/page.tsx
+++ frontend/src/app/(routes)/form-jhonatan/page.tsx
@@ -1,15 +1,11 @@
+import FormTailwind from '@/components/FormTailwind/FormTailwind';
+
+
 const Form = () => {
   return (
-    <div className='mx-auto mt-10 max-w-4xl border px-4 py-3'>
-      <h1 className='text-2xl font-bold'>Form</h1>
-      <p className='text-gray-500'>This is a form page.</p>
-      <form className='mt-4'>
-        <label htmlFor="name" className='block text-sm font-medium text-gray-700'>Name</label>
-        <input type="text" id="name" name="name" className='mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500' />
-        <button type="submit" className='mt-4 bg-blue-500 text-white px-4 py-2 rounded-md'>Submit</button>
-      </form>
+    <div className='mx-auto mt-10 max-w-4xl border rounded-2xl px-4 py-3'>
+      <FormTailwind/>
     </div>
   )
 }
-
 export default Form;
